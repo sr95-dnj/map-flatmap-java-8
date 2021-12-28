c6f97f77d05550171a717dcf7b77f132770e89a9
siddiquer95 <siddikur.rahman@ibcs-primax.com>
2021-12-28 23:15:13 +0600

    initial commit

---
+++ src/main/java/com/sr/mapflatmap/MapFlatMapExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sr.mapflatmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapFlatMapExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapFlatMapExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sr/mapflatmap/MapFlatMapExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sr.mapflatmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapFlatMapExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
